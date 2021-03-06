4eacc1614f7a5c4ccd9cc97d8ca6f03c5f665829
conv22 <metoneup@gmail.com>
2021-03-06 10:40:18 +0300

    devtools removed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -8,9 +8,9 @@ const middleware = [thunk];
 const store = createStore(
   RootReducer,
   compose(
-    applyMiddleware(...middleware),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    //   (window as any).__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
